d6b400883f5f2844725af53ba711b1065c0f1c0f
Jeffrey <jeffchan4@icloud.com>
2024-06-28 14:44:15 -0400

    fetch from NIH, initial query

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
-
 
 // Parse JSON bodies
 app.use(express.json());
@@ -15,20 +14,118 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+function getDateTime(){
+    function pad(number) {
+        return (number < 10 ? '0' : '') + number;
+    }
+
+    const currentDate = new Date();
+    // Format the date and time
+    const formattedDateTime = `${currentDate.getFullYear()}-${pad(currentDate.getMonth() + 1)}-${pad(currentDate.getDate())} ${pad(currentDate.getHours())}:${pad(currentDate.getMinutes())}:${pad(currentDate.getSeconds())}`;
+
+    return formattedDateTime;
+}
+
+
+//put in db and deploy. maybe use microsoft.
+let tasks= {}
+
+
+
+const search_term= async (taskId) => {
+    
+
+    return new Promise ((resolve)=>{
+        finish_time= Date.now()
+        tasks[taskId].status='completed';
+        tasks[taskId].run_seconds= finish_time - tasks[taskId].start_time;
+        resolve();
+    })
+}
+
+
+app.get('/api/search', (req,res) => {
+    const db= 'pubmed';
+    const term= 'diabetes';
+
+    axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi`, { params: { db, term } })
+        .then(response => {
+            res.json(response.data);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+function test () {
+    const db= 'pubmed';
+    const term= 'diabetes';
+
+    axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi`, { params: { db, term } })
+        .then(response => {
+            const responseType= typeof response.data;
+            console.log(responseType)
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
 
 //Create task_id for query while it runs in the background
 app.post('/search/:term', (req,res)=>{
     const pubmed_term = req.params.term;
-
-
-    res.json({
+    
+    //write logic to see if valid pub med term then send 
+    response={
         "records": 212111,
         "query": "alzheimer disease",
         "task_id": "7fd381bf3cbe28e892e163db81b9e2cd"
-        })
+        }
+    tasks[response['task_id']]= {
+        "task_id": response['task_id'],
+        "status":"processing",
+        "result":{},
+        "created_time": getDateTime(),
+        "start_time": Date.now(),
+        "run_seconds": 0
+        }
+    search_term(response['task_id'])
+    res.json(response)
 })
+
+app.get('/fetch/:task_id', (req,res)=>{
+    const task_id = req.params.task_id;
+    const task= tasks[task_id]
+    if (!task){
+        return res.status(404).json({error:'task not found'})
+    }
+    if (task['status']=='processing'){
+        res.json({
+            "task_id": task['task_id'],
+            "status": task['status'],
+            "created_time": task['created_time']
+        })
+    } else if (task['status']=='completed'){
+        res.json({
+            "task_id": task['task_id'],
+            "status": task['status'],
+            "result":{
+            "pmids":[7952237,37506310,32397415]
+            },
+            "created_time":task['created_time'],
+            "run_seconds": task['run_seconds']
+        })
+    
+    }
+});
+
+
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+test();
+
